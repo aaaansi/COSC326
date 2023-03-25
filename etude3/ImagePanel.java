78ab7c0b697a985dd9a18bb6e8925bb8aa62c575
Hamzah Alansi <alaha971@student.otago.ac.nz>
2023-03-25 18:34:28 +1300

    Implemented a zoom in feature

--- etude3/ImagePanel.java
+++ etude3/ImagePanel.java
@@ -1,31 +1,83 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.awt.geom.*;
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
 
 /**
  * https://medium.com/@mojca.rojko/drawing-a-koch-snowflake-in-java-3268cbed94c8
  * Algorithm for drawing the snowflake from ^^
  */
 
-public class ImagePanel extends JPanel {
+public class ImagePanel extends JPanel implements MouseWheelListener, MouseListener, MouseMotionListener {
 
   private int levels = 1;
 
+  private double zoomFactor = 1;
+  private double prevZoomFactor = 1;
+  private boolean zoomer;
+  private boolean dragger;
+  private boolean released;
+  private double xOffset = 0;
+  private double yOffset = 0;
+  private int xDiff;
+  private int yDiff;
+  private Point startPoint;
+
   public ImagePanel() {
     setPreferredSize(new Dimension(345, 429));
+    addMouseWheelListener(this);
+    addMouseMotionListener(this);
+    addMouseListener(this);
   }
 
   @Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     g.setColor(Color.BLACK);
+    Graphics2D g2d = (Graphics2D) g;
 
     double panelWidth = getWidth();
     double panelHeight = getHeight();
+    if (zoomer) {
+      AffineTransform at = new AffineTransform();
 
-    Graphics2D g2d = (Graphics2D) g;
-    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+      double xRel = MouseInfo.getPointerInfo().getLocation().getX() - getLocationOnScreen().getX();
+      double yRel = MouseInfo.getPointerInfo().getLocation().getY() - getLocationOnScreen().getY();
+
+      double zoomDiv = zoomFactor / prevZoomFactor;
+
+      xOffset = (zoomDiv) * (xOffset) + (1 - zoomDiv) * xRel;
+      yOffset = (zoomDiv) * (yOffset) + (1 - zoomDiv) * yRel;
+
+      at.translate(xOffset, yOffset);
+      at.scale(zoomFactor, zoomFactor);
+      prevZoomFactor = zoomFactor;
+      g2d.transform(at);
+      zoomer = false;
+    }
+
+    if (dragger) {
+      AffineTransform at = new AffineTransform();
+      at.translate(xOffset + xDiff, yOffset + yDiff);
+      at.scale(zoomFactor, zoomFactor);
+      g2d.transform(at);
+
+      if (released) {
+        xOffset += xDiff;
+        yOffset += yDiff;
+        dragger = false;
+      }
+
+    }
+    // g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+    // RenderingHints.VALUE_ANTIALIAS_ON);
+    // g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
+    // RenderingHints.VALUE_STROKE_PURE);
 
     drawKochSnowflake(g2d, levels, panelWidth / 2 - ((panelWidth / 2) - 50), panelHeight / 2,
         panelWidth / 2 + ((panelWidth / 2) - 50), panelHeight / 2);
@@ -72,5 +124,64 @@ public class ImagePanel extends JPanel {
   public void setLevels(int level) {
     this.levels = level;
     repaint();
+  }
+
+  @Override
+  public void mouseWheelMoved(MouseWheelEvent e) {
+
+    zoomer = true;
+
+    // Zoom in
+    if (e.getWheelRotation() < 0) {
+      zoomFactor *= 1.1;
+      repaint();
+    }
+    // Zoom out
+    if (e.getWheelRotation() > 0) {
+      zoomFactor /= 1.1;
+      repaint();
+    }
+  }
+
+  @Override
+  public void mouseDragged(MouseEvent e) {
+    Point curPoint = e.getLocationOnScreen();
+    xDiff = curPoint.x - startPoint.x;
+    yDiff = curPoint.y - startPoint.y;
+
+    dragger = true;
+    repaint();
+
+  }
+
+  @Override
+  public void mouseMoved(MouseEvent e) {
+  }
+
+  @Override
+  public void mouseClicked(MouseEvent e) {
+
+  }
+
+  @Override
+  public void mousePressed(MouseEvent e) {
+    released = false;
+    startPoint = MouseInfo.getPointerInfo().getLocation();
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent e) {
+    released = true;
+    repaint();
+  }
+
+  @Override
+  public void mouseEntered(MouseEvent e) {
+
+  }
+
+  @Override
+  public void mouseExited(MouseEvent e) {
+
   }
 }
