c4df097e89492eefdcf590bb8f7a0ae364d8ddea
Hamzah Alansi <alaha971@student.otago.ac.nz>
2023-05-12 18:52:17 +1200

    Changes to myOwn

--- etude9/myOwn.c
+++ etude9/myOwn.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-#define MAX_LINE_LEN 1024
-#define MAX_FIELD_LEN 256
+#define MAX_FIELD_LEN 100
+#define MAX_LINE_LEN 256
 
 typedef struct
 {
-    char first_name[MAX_FIELD_LEN];
-    char last_name[MAX_FIELD_LEN];
-    char email[MAX_FIELD_LEN];
-    char phone[MAX_FIELD_LEN];
+    char *first_name;
+    char *last_name;
+    char *email;
+    char *phone;
 } Client;
 
 void search_by_field(char field, char *value, FILE *fp)
@@ -21,39 +22,63 @@ void search_by_field(char field, char *value, FILE *fp)
 
     while (fgets(line, MAX_LINE_LEN, fp))
     {
-        sscanf(line, "%s %s %s %s", client.first_name, client.last_name, client.email, client.phone);
+        client.first_name = (char *)malloc(MAX_FIELD_LEN * sizeof(char));
+        client.last_name = (char *)malloc(MAX_FIELD_LEN * sizeof(char));
+        client.email = (char *)malloc(MAX_FIELD_LEN * sizeof(char));
+        client.phone = (char *)malloc(MAX_FIELD_LEN * sizeof(char));
+
+        sscanf(line, "%s %s %s %s", client.first_name, client.last_name, client.phone, client.email);
+
+        // Convert the search value and client field to lowercase
+        char lower_value[MAX_FIELD_LEN];
+        char lower_field[MAX_FIELD_LEN];
+        strcpy(lower_value, value);
+        strcpy(lower_field, value);
+        for (int i = 0; lower_value[i]; i++)
+        {
+            lower_value[i] = tolower(lower_value[i]);
+        }
+        for (int i = 0; lower_field[i]; i++)
+        {
+            lower_field[i] = tolower(lower_field[i]);
+        }
 
         switch (field)
         {
         case 'f':
-            if (strcmp(client.first_name, value) == 0)
+            if (strcasecmp(client.first_name, lower_value) == 0)
             {
                 printf("%s", line);
                 found = 1;
             }
             break;
         case 'l':
-            if (strcmp(client.last_name, value) == 0)
+            if (strcasecmp(client.last_name, lower_value) == 0)
             {
                 printf("%s", line);
                 found = 1;
             }
             break;
         case 'e':
-            if (strcmp(client.email, value) == 0)
+            if (strcasecmp(client.email, lower_value) == 0)
             {
                 printf("%s", line);
                 found = 1;
             }
             break;
         case 'p':
-            if (strcmp(client.phone, value) == 0)
+            if (strcasecmp(client.phone, lower_value) == 0)
             {
                 printf("%s", line);
                 found = 1;
             }
             break;
         }
+
+        free(client.first_name);
+        free(client.last_name);
+        free(client.email);
+        free(client.phone);
     }
 
     if (!found)
@@ -83,18 +108,53 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    printf("Enter f to search by first name: \n      l to search by last name: \n      e to search by email: \n      p to search by phone number: \n      x to Exit:\n");
-    scanf("%c", &search_field);
-    if (search_field == 'x')
+    int valid_input = 0;
+    while (!valid_input)
     {
-        printf("Exit Success!");
-        return 0;
+        printf("Enter f to search by first name:\n"
+               "      l to search by last name:\n"
+               "      e to search by email:\n"
+               "      p to search by phone number:\n"
+               "      x to Exit:\n");
+        scanf(" %c", &search_field);
+
+        if (search_field == 'x')
+        {
+            printf("Exit Success!");
+            return 0;
+        }
+
+        printf("Enter search value: ");
+        scanf("%s", search_value);
+
+        // Convert the search value to lowercase
+        for (int i = 0; search_value[i]; i++)
+        {
+            search_value[i] = tolower(search_value[i]);
+        }
+
+        search_by_field(search_field, search_value, fp);
+
+        printf("\n");
+
+        // Prompt for another search if the input is invalid
+        printf("Do you want to perform another search? (y/n): ");
+        char choice[10];
+        scanf("%9s", choice);
+
+        if (tolower(choice[0]) != 'y')
+        {
+            valid_input = 1;
+        }
+
+        // Flush the input buffer
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF)
+        {
+        }
+
+        fseek(fp, 0, SEEK_SET); // Reset the file pointer to the beginning of the file
     }
-
-    printf("Enter search value: ");
-    scanf("%s", search_value);
-
-    search_by_field(search_field, search_value, fp);
 
     fclose(fp);
     return 0;
